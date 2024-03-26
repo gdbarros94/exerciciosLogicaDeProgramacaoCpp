b5c88aeb6686c6e1255bc3d737e145266a180125
Gabriel Pires de Barros <gdbarros95@gmail.com>
2024-03-26 14:51:23 +0000

    feito

---
+++ exercicio1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    
+    printf("A soma dos numeros e: %d\n", num1 + num2);
+    
+    return 0;
+}
---
+++ exercicio3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2, num3;
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &num1);
+    
+    printf("Digite o segundo numero: ");
+    scanf("%f", &num2);
+    
+    printf("Digite o terceiro numero: ");
+    scanf("%f", &num3);
+    
+    printf("A media dos numeros e: %.2f\n", (num1 + num2 + num3) / 3);
+    
+    return 0;
+}
